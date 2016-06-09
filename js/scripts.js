11ab7f39b77b76683cec3c3f743ff1a141e54d03
Maldon Meehan and Sky Rousse <maldon@>
2016-06-09 11:48:12 -0700

    add function to compare arrays and find values to be returned

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 // JavaScript/jQuery Business Logic
-
-
-
 var romanNumerals =["M", "CM", "DM", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+var roman ="";
 
-function convert(inputNumber) {
-
+var converter = function convert(inputNumber) {
+  debugger;
+  for( var i = 0; numbers.length; i++) {
+    while(inputNumber > numbers[i]) {
+      roman = roman + romanNumerals[i];
+      inputNumber = inputNumber - numbers[i];
+    }
+  }
+  return roman;
 };
 
 // JavaScript/jQuery Front-End Logic
@@ -14,10 +19,13 @@ $(document).ready(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
     var inputNumber = $("input#input-number").val();
-    debugger;
+
     if (inputNumber <= 0 || inputNumber >= 4000) {
       alert("number must a value between 1 and 3999");
     }
     //call function
+    converter(inputNumber);
+
+    //add output
   });
 });
