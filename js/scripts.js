2abf7a978a800dd40fd0ee272c66af95a6ecabba
Maldon Meehan and Sky Rousse <maldon@>
2016-06-09 11:05:50 -0700

    add conditional to evaluate for numbers less than or equal to zero

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // JavaScript/jQuery Business Logic
 
+
+
 var romanNumerals =["M", "CM", "DM", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
@@ -12,6 +14,10 @@ $(document).ready(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
     var inputNumber = $("form#converter").val();
+    if (inputNumber <= 0) {
+      alert("number must be greater than 0");
+    };
+    
     //call function
   });
 });
